f2a1bbeaeab2300b6d3e5d41248763e872b5aec3
Diego Morales <diego.morales@gmx.net>
2018-03-14 10:29:33 +0100

    update docker setup

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-docker-compose up -d
-docker-compose exec web bash
+IMG_NAME=static-site-setup
+
+if [[ "$(docker images -q $IMG_NAME 2> /dev/null)" == "" ]]; then
+  docker build -t $IMG_NAME .
+fi
+
+docker run -it -p 3000:3000 -p 3001:3001 -v $(pwd):/home/node/code $IMG_NAME bash
